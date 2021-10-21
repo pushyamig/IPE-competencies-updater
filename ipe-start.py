fb9304158deac252716ec160fd314ceae4ee3e07
pushyami <pushyami@umich.edu>
2021-10-21 13:48:48 -0400

    base setup with api directory

--- ipe-start.py
+++ ipe-start.py
@@ -5,6 +5,7 @@ from read_env_props import ReadEnvProps
 from gspread.models import Worksheet
 from ipe_course_data.get_ipe_data_from_gsheets import GetIPEDataFromSheets
 from ipe_process_orchestrator.orchestrator import IPECompetenciesOrchestrator
+from umich_api.api_utils import ApiUtil
 
 load_dotenv()
 
@@ -13,10 +14,10 @@ logging.basicConfig(level=os.getenv('LOG_LEVEL') if os.getenv('LOG_LEVEL') else 
 def main():
     logger.info("IPE Process Starting....")
     envProps: ReadEnvProps =  ReadEnvProps()
-    logger.info(envProps.get_env_props())
     ipeData: GetIPEDataFromSheets=GetIPEDataFromSheets(envProps.get_env_props())
     worksheet: Worksheet = ipeData.get_data()
     worksheet_dataframe: pd.DataFrame  = pd.DataFrame(worksheet.get_all_records())
+    ApiUtil
     orchestrator: IPECompetenciesOrchestrator = IPECompetenciesOrchestrator(worksheet_dataframe, envProps.get_env_props())
     orchestrator.start_composing_process()
 
